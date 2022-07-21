33c743260f5004e690592f53700bbc74c1f96d77
voldergit <voldergit@gmail.com>
2022-07-21 22:04:26 +0300

    footer, new-items.html

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # Marketplace
+
+jquery
+mixit
+slick-slider
+RateYo
+rangeslider
+
+CSS Sprites Generator
+https://icomoon.io/app/
--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,7 @@
 $(function () {
-  var mixer = mixitup(".products__inner-box");
+  if ($(".products__inner-box").length) {
+    var mixer = mixitup(".products__inner-box");
+  }
 
   $(".rate-star").rateYo({
     rating: 5,
@@ -11,5 +13,13 @@ $(function () {
     arrows: false,
     slidesToShow: 4,
     slidesToScroll: 4,
+  });
+  $(".js-range-slider").ionRangeSlider({
+    type: "double",
+    min: 0,
+    max: 1000,
+    from: 200,
+    to: 500,
+    grid: true,
   });
 });
